8b035d46edd08b6065d8d29509983214d315f156
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-07-25 09:33:00 +0200

    Add first visualization, cost of project

--- visualization.js
+++ visualization.js
@@ -1,9 +1,32 @@
+const draw = _data => {
+    const costs = _data.map(row => parseInt(row.totalCost));
+    costs.sort(d3.descending);
+    const data = costs;
+
+
+    var width = 420,
+        barHeight = 5;
+
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, width]);
+
+    var chart = d3.select(".chart")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
+
+    var bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+}
+
 const onDomLoaded = () => {
-    d3.csv('./h2020.csv', data => { console.log(data) });
-//
-//    const promise = fetch('./h2020.csv')
-//          .then(response => response.text())
-//          .then(console.log)
-//          .catch(e => console.log)
+    d3.csv('./h2020.csv').then(draw);
 }
 document.addEventListener("DOMContentLoaded", onDomLoaded);
